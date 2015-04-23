3f194f3d6f266e7abc55e7ddfd2e3c6819d9da34
Altug Sahin <altug@aecoffice.com>
2015-04-23 10:55:29 -0400

    changed folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 
 gulp.task('sass', function() {
-    return gulp.src('lib/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist'));
+  return gulp.src('lib/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-    //gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('lib/**/*.scss', ['sass']);
+  gulp.watch('lib/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass', 'watch']);
