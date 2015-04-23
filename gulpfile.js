347637e01030f3fa88a3cdd798a0a5184f75c87b
Altug Sahin <altug@aecoffice.com>
2015-04-23 12:43:43 -0400

    build task for test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
+var runSequence = require('run-sequence');
 
+
+gulp.task('clean', function(cb) {
+  return del('dist/**', cb);
+});
+
+gulp.task('test-html', function(cb) {
+  return gulp.src(['test/**'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('font', function() {
+  return gulp.src('lib/font/**/*')
+    .pipe(gulp.dest('dist/font'));
+});
 
 gulp.task('sass', function() {
   return gulp.src('lib/sass/**/*.scss')
@@ -8,8 +24,27 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('test', function(cb) {
+  runSequence(
+    ['clean'], ['font', 'sass', 'test-html', 'watch-test'],
+    cb
+  );
+});
+
 gulp.task('watch', function() {
   gulp.watch('lib/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('watch-test', function() {
+  gulp.watch('lib/sass/**/*.scss', ['sass']);
+  gulp.watch(['test/**'], ['test-html']);
+});
+
+gulp.task('build', function(cb) {
+  runSequence(
+    ['clean'], ['clean', 'font', 'sass', 'watch'],
+    cb
+  );
+});
+
+gulp.task('default', ['build']);
